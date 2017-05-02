0bff658efed811e5c5526b39a374d9c0154ba5e1
Erik Reid <erik.reid@geant.org>
2017-05-02 14:24:41 +0200

    changed module list

--- setup.py
+++ setup.py
@@ -13,21 +13,20 @@ setup(
     name='terraformware',
     version='0.9.1',
     description=("Tool for managing server status on Terraform and Infoblox"),
-    long_description=read('README.md'),
+    long_description="long description: cf. http://stackoverflow.com/a/761847",
     url='https://git.geant.net/gitlab/terraform/terraformware',
     author='Massimiliano Adamo',
     author_email='massimiliano.adamo@geant.org',
     license='GPL',
-    include_package_data=True,
     scripts=glob('bin/*'),
-    packages=['iblox_kit'] + [os.path.join("iblox_kit", p) for p in find_packages("iblox_kit")],
+    packages=['iblox_kit'],
     zip_safe=False
     )
 
-for cleanup in ['dist', 'build', 'terraformware.egg-info']:
-    if os.access(cleanup, os.W_OK):
-        remove_tree(cleanup, verbose=1, dry_run=0)
-
-print "removing stale bytecode"
-for pyc in glob('iblox_kit/*.pyc'):
-    os.unlink(pyc)
+#for cleanup in ['dist', 'build', 'terraformware.egg-info']:
+#    if os.access(cleanup, os.W_OK):
+#        remove_tree(cleanup, verbose=1, dry_run=0)
+#
+#print "removing stale bytecode"
+#for pyc in glob('iblox_kit/*.pyc'):
+#    os.unlink(pyc)
